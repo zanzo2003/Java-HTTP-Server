c7a9cec9c28183b7c185866163c0af249c90dff1
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-06-27 21:09:03 +0530

    setup: setting up my own http server

---
+++ src/main/java/net/bhaskarshashwath/httpserver/HttpServer.java
@@ -0,0 +1,14 @@
+package net.bhaskarshashwath.httpserver;
+
+
+/**
+ * Driver class of the http server
+ * **/
+
+public class HttpServer {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!!!");
+    }
+}
