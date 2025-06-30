a6ba96bf2fa7fc3b2839abeee42e83ea67039999
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-06-30 19:30:05 +0530

    feat: serving static HTML content from HTTP server

--- src/main/java/net/bhaskarshashwath/httpserver/HttpServer.java
+++ src/main/java/net/bhaskarshashwath/httpserver/HttpServer.java
@@ -1,9 +1,14 @@
 package net.bhaskarshashwath.httpserver;
 
 
+import net.bhaskarshashwath.httpserver.config.Configuration;
 import net.bhaskarshashwath.httpserver.config.ConfigurationManager;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Driver class of the http server
@@ -15,7 +20,39 @@ public class HttpServer {
 
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Hello World!!!");
+        System.out.println("Starting server ....");
         ConfigurationManager.getInstance().loadConfigurationFile(CONFIGURATION_FILE_PATH);
+        Configuration configuration = ConfigurationManager.getInstance().getCurrentConfiguration();
+        System.out.println("Port : " + configuration.getPort());
+        System.out.println("Webroot : " + configuration.getWebroot());
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(configuration.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html> <head> <title>Java HTTP Server</title> </head> <body> <h1>Simple JAVA HTTP server</h1> </body></html>";
+            final String CRLF = "\r\n";
+            byte[] content = html.getBytes("UTF-8");
+            String response = "HTTP/1.1 200 OK" + CRLF +
+                    "Content-Type: text/html; charset=UTF-8" + CRLF +
+                    "Content-Length: " + content.length + CRLF +
+                    "Connection: keep-alive" + CRLF +
+                    CRLF;
+
+            outputStream.write(response.getBytes("UTF-8"));
+            outputStream.write(content);
+            outputStream.flush();
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        }catch(IOException e){
+            throw new IOException();
+        }
     }
 }
