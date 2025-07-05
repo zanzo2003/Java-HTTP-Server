dc2ac286be2708240f5ff85d836ab95b5205f18f
Shashwath Bhaskar <110276216+zanzo2003@users.noreply.github.com>
2025-07-05 20:35:57 +0530

    feat: request line parser

--- src/main/java/net/bhaskarshashwath/httpserver/http/HttpParser.java
+++ src/main/java/net/bhaskarshashwath/httpserver/http/HttpParser.java
@@ -4,6 +4,7 @@ package net.bhaskarshashwath.httpserver.http;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -11,6 +12,12 @@ import java.nio.charset.StandardCharsets;
 public class HttpParser {
 
     private static final Logger logger = LoggerFactory.getLogger(HttpParser.class);
+
+
+    private static final int SP = 0x20;  // 32
+    private static final int CR = 0x8D;  // 13
+    private static final int LF = 0x8A;  //10
+
 
     public HttpRequest parseHttpRequest(InputStream inputStream){
 
@@ -28,7 +35,19 @@ public class HttpParser {
     }
 
 
-    private void parseRequestLine(InputStreamReader reader, HttpRequest request){}
+    private void parseRequestLine(InputStreamReader reader, HttpRequest request) throws IOException {
+
+        int _byte;
+
+        while( (_byte = reader.read() )>= 0){
+            if(_byte == CR ){
+                _byte = reader.read();
+                if(_byte == LF){
+                    return ;
+                }
+            }
+        }
+    }
 
     private void parseHeader(InputStreamReader reader, HttpRequest request){}
 
